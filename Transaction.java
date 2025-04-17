e20c2ade2d48b52050c1321e3d570e8bd9e2509e
Your Name <you@example.com>
2025-04-17 22:36:08 +0530

    transaction

---
+++ Transaction.java
@@ -0,0 +1,45 @@
+public class Transaction {
+    public static void main(String[] args) {
+        BankAccount account = new BankAccount("XYZ", "01");
+
+        account.deposit(500);
+        account.withdraw(200);
+        account.getPreviousTransaction(); // Should print last transaction
+    }
+}
+
+class BankAccount {
+    int balance;
+    int previousTransaction;
+    String customerName;
+    String customerId;
+
+    BankAccount(String cname, String cid) {
+        customerName = cname;
+        customerId = cid;
+    }
+
+    void deposit(int amount) {
+        if (amount != 0) {
+            balance += amount;
+            previousTransaction = amount;
+        }
+    }
+
+    void withdraw(int amount) {
+        if (amount != 0) {
+            balance -= amount;
+            previousTransaction = -amount;
+        }
+    }
+
+    void getPreviousTransaction() {
+        if (previousTransaction > 0) {
+            System.out.println("Deposited: " + previousTransaction);
+        } else if (previousTransaction < 0) {
+            System.out.println("Withdrawn: " + Math.abs(previousTransaction));
+        } else {
+            System.out.println("No transaction has occurred!");
+        }
+    }
+}
